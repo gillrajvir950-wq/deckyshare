5996d2bd07bc2b2413152f321cbc5ee5afd142d0
gillrv <gillrajvir950@gmail.com>
2026-09-13 16:26:57 +0200

    Wire RC1 hardening runtime

--- exactly_once_bootstrap.py
+++ exactly_once_bootstrap.py
@@ -4,6 +4,8 @@
 def install(core):
     from exactly_once import install as install_exactly_once
     from restart_hardening import install as install_restart_hardening
+    from rc1_hardening import install as install_rc1_hardening
 
     install_exactly_once(core)
     install_restart_hardening(core)
+    install_rc1_hardening(core)
