092ed17400b732d89c06270dec852ce2f625fff5
gillrv <gillrajvir950@gmail.com>
2026-09-13 16:18:09 +0200

    Clean RC1 bootstrap naming and server version

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import core_main as _core
-from exactly_once_bootstrap import install as _install_exactly_once
+from exactly_once_bootstrap import install as _install_reliability
 
-_install_exactly_once(_core)
+_install_reliability(_core)
+_core.Handler.server_version = "DeckyShare/1.1.0-rc1"
 
 
 class Plugin(_core.Plugin):
